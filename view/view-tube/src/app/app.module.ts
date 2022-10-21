b5c5728f006c9b783f24a99839b9b5fc8a06c45f
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2022-10-21 22:14:37 +0530

    completed

---
+++ view/view-tube/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'; 
+
+import { AppComponent } from './app.component';
+import { ChipsBarComponent } from './chips-bar/chips-bar.component';
+import { VideoContentComponent } from './video-content/video-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VideoContentComponent,
+    ChipsBarComponent,
+    NavigationBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
